9432e7223a7a2d90f122bf8963ce0bf0022a0d2c
droper <pedro.munoz@tas'screenit.com>
2025-02-08 17:08:00 -0500

    Optimize code

--- transmitter.py
+++ transmitter.py
@@ -13,8 +13,6 @@ import os
 import argparse
 import logging
 
-from functools import partial
-
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
